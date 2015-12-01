b945e454906fa1fe695d6ba47e2c2d8803f4c967
Tempurturtul <tempurturtul@gmail.com>
2015-12-01 15:42:03 -0500

    feat: Display fun thoughts
    Part of JavaScript Basics: Lesson 1 - Data Types.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// $('#main').append('Matthew');
+// [string].replace([old], [new]);
+
+var awesomeThoughts = "My name is Matthew and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+$('#main').append(funThoughts);
