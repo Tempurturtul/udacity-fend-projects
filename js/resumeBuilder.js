be8f7c69d6ec2c6a172795c7bbb862e4224e4621
Tempurturtul <tempurturtul@gmail.com>
2015-12-03 11:50:58 -0500

    Finished Lesson 1 - Data Types.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,77 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name" : "Matthew Feidt",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "717-303-9839",
+    "email" : "tempurturtul@gmail.com",
+    "github" : "Tempurturtul",
+    "twitter" : "@tempurturtul",
+    "location" : "Brasher Falls, NY"
+  },
+  "bioPic" : "images/self.jpg",
+  "welcomeMessage" : "Welcome to my work-in-progress resume!",
+  "skills" : [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "jQuery",
+    "NodeJS",
+    "AngularJS",
+    "Git",
+    "Gulp",
+    "Vagrant"
+  ]
+};
 
-// $('#main').append('Matthew');
-// [string].replace([old], [new]);
+var education = {
+  "schools": [
+    {
+      "name": "Central Penn College",
+      "dates": "2008 - 2011",
+      "location": "Harrisburg, PA",
+      "degree": "BS",
+      "majors": ["Security Management"],
+      "url": "http://www.centralpenn.edu/"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JavaScript Basics",
+      "school": "Udacity",
+      "dates": "2015",
+      "url": "https://www.udacity.com/course/javascript-basics--ud804"
+    }
+  ]
+};
 
-var awesomeThoughts = "My name is Matthew and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+var work = {
+  "jobs": [
+    {
+      "title": "Security Officer",
+      "employer": "Government",
+      "dates": "2012 - Current",
+      "location": "Massena, NY",
+      "description": "Sensitive security work requiring routine background " +
+      "investigations, strict adherence to Standard Operating Procedures, " +
+      "and regular professional interaction with both stakeholders and the " +
+      "public. Supervisory and peer-mentoring roles fulfilled on multiple " +
+      "occasions at the request of managment."
+    }
+  ]
+};
 
-$('#main').append(funThoughts);
+var projects = {
+  "projects": [
+    {
+      "title": "Portfolio",
+      "dates": "November 2015",
+      "description": "A web development portfolio built as the first project " +
+      "in Udacity's Front-End Web Development Nanodegree program.",
+      "images": [
+        "images/fend-portfolio-top.png",
+        "images/fend-portfolio-work.png",
+        "images/fend-portfolio-contact.png"
+      ]
+    }
+  ]
+};
