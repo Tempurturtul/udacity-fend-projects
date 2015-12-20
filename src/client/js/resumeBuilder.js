ce6badbb713137dca34ca73ebf0b6c6d9de9b243
Tempurturtul <tempurturtul@gmail.com>
2015-12-20 08:36:36 -0500

    Added course, styling.

--- src/client/js/resumeBuilder.js
+++ src/client/js/resumeBuilder.js
@@ -107,6 +107,13 @@ var education = {
       'description': '"Learn many of the concepts that underlie all programming languages. Use functional programming and contrast it with object-oriented programming. Through experience writing programs and studying three different languages, learn the key issues in designing and using programming languages, such as modularity and the complementary benefits of static and dynamic typing. This course is neither particularly theoretical nor just about programming specifics – it will give you a framework for understanding how to use language constructs effectively and how to design correct and elegant programs. By using different languages, you learn to think more deeply than in terms of the particular syntax of one language. The emphasis on functional programming is essential for learning how to write robust, reusable, composable, and elegant programs – in any language."'
     },
     {
+      'title': 'Workshoppers',
+      'school': 'NodeSchool',
+      'date': 2015,
+      'url': 'http://nodeschool.io/#workshopper-list',
+      'description': 'Learn essential skills for working with Node.js. Various topics covered such as Git and Github, asynchronous i/o, http, npm modules, streams, express.js framework basics, promises, MongoDB basics, debugging skills, and testing.'
+    },
+    {
       'title': 'JavaScript Basics',
       'school': 'Udacity',
       'date': 2015,
