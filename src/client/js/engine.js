b8f6d16d47099005ebb20d328c4b48bb48e0419b
Tempurturtul <tempurturtul@gmail.com>
2016-01-28 18:55:40 -0500

    Styling fix.

--- src/client/js/engine.js
+++ src/client/js/engine.js
@@ -27,7 +27,15 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+
+    /**********************
+    * BEGIN My Code
+    **********************/
+    // doc.body.appendChild(canvas);
+    doc.getElementsByTagName('main')[0].appendChild(canvas);
+    /**********************
+    * END My Code
+    **********************/
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
