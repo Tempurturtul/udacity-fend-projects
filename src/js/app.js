db83cab38339caa9e3343d8f72174fe23e49fb37
Tempurturtul <tempurturtul@gmail.com>
2016-02-29 16:53:33 -0500

    Setting  up.

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,25 @@
-var map;
+(function(global) {
+  var ko = global.ko,
+      google = global.google,
+      localStorage = global.localStorage;
 
-/**
- * Initializes the Google Map. (Invoked by the callback on the Google Maps API url.)
- */
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+  // Marker Model
+  function Marker(data) {
+    this.draggable = ko.observable(data.draggable);
+    this.icon = ko.observable(data.icon);
+    this.label = ko.observable(data.label);
+    this.place = ko.observable(data.place);
+    this.position = ko.observable(data.position);
+    this.title = ko.observable(data.title);
+    this.visible = ko.observable(data.visible);
+  }
+
+  // Application View Model
+  function AppViewModel() {
+    var self = this;
+  };
+
+  // TODO Custom binding for map. (http://stackoverflow.com/a/16305965)
+
+  ko.applyBindings(new AppViewModel());
+})(this);
