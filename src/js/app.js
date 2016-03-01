df16341df16568b30582dd8c09617e4c05492c0b
Tempurturtul <tempurturtul@gmail.com>
2016-03-01 15:24:01 -0500

    Map, marker, and marker list initialization.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
+// Knockout-controlled functionality.
+
 (function(global) {
   var ko = global.ko,
       localStorage = global.localStorage,
       map = global.map;
 
-  
+  // Marker Model
+  function Marker(data) {
+    this.position = ko.observable(data.position);
+    this.title = ko.observable(data.title);
+  }
+
+  // App View Model
+  function AppViewModel() {
+    var self = this;
+
+    self.markerList = ko.observableArray([]);
+
+    map.markersData.forEach(function(data) {
+      self.markerList.push(new Marker(data));
+    });
+  }
+
+  ko.applyBindings(new AppViewModel());
 })(this);
--- src/js/map.js
+++ src/js/map.js
@@ -1,22 +1,56 @@
+// Google Maps functionality.
+
 (function(global) {
   var document = global.document,
       localStorage = global.localStorage,
       google = global.google,
       map,
-      defaultMapOptions = {
-        center: new google.maps.LatLng(35.689, 139.692), // Tokyo, Japan.
-        zoom: 10
-      },
-      mapOptions = JSON.parse(localStorage.getItem('mapOptions')) || defaultMapOptions;
+      markers = [],  // Actual Google Markers.
+      markersData;   // Data defining Google Markers.
 
-  init(mapOptions);
+  init();
 
   /**
-   * Initializes the Google Map on the #map element with the given Google MapOptions.
+   * Initializes the Google Map and Markers.
    */
-  function init(mapOptions) {
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  }
+  function init() {
+    // Defaults to use if local storage is empty.
+    var defaultMapOptions = {
+          center: new google.maps.LatLng(35.689, 139.692), // Tokyo, Japan.
+          zoom: 10
+        },
+        defaultMarkersData = [
+          {
+            position: new google.maps.LatLng(35.6738167, 139.7623955),
+            title: 'Default 1'
+          },
+          {
+            position: new google.maps.LatLng(35.6738167, 139),
+            title: 'Default 2'
+          },
+          {
+            position: new google.maps.LatLng(35.6738167, 139.8),
+            title: 'Default 3'
+          },
+        ];
 
-  global.map = {};
+    // Local storage values or defaults.
+    var mapOptions = JSON.parse(localStorage.getItem('mapOptions')) || defaultMapOptions;
+
+    markersData = JSON.parse(localStorage.getItem('markersData')) || defaultMarkersData;
+
+    // Initialize the map.
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Add the markers.
+    markersData.forEach(function(data) {
+      data.map = map;
+      var marker = new google.maps.Marker(data);
+      markers.push(marker);
+    });
+  }
+  
+  global.map = {
+    markersData: markersData
+  };
 })(this);
