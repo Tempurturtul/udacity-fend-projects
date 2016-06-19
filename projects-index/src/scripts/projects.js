73683354853118f972d8595cd7a9816478ae9e02
Tempurturtul <tempurturtul@gmail.com>
2016-06-19 10:17:08 -0400

    Update projects-index site to include links to source for all projects
    The build-script's behavior has changed to consider all non-hidden
    directories other than its own as projects, and the resulting
    projects.js file has also changed to indicate the presence of a
    live site for each project.
    
    Also in this commit are minor css changes.

--- projects-index/src/scripts/projects.js
+++ projects-index/src/scripts/projects.js
@@ -1,2 +1,2 @@
 // Generated automatically by `build-script`.
-var projects = ['fend-neighborhood-map','fend-frogger','fend-portfolio','fend-resume','fend-optimization'];
+var projects = [{name: 'fend-neighborhood-map', live: true}, {name: 'fend-design-patterns', live: false}, {name: 'fend-feed-reader', live: false}, {name: 'fend-frogger', live: true}, {name: 'fend-portfolio', live: true}, {name: 'fend-resume', live: true}, {name: 'fend-optimization', live: true}];
