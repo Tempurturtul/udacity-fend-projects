0a2b9e76c2a25a01ea5f63b7ec283d9217d2f053
Tempurturtul <tempurturtul@gmail.com>
2016-07-01 17:54:18 -0400

    Remove automatically generated files

--- projects-index/src/scripts/projects.js
+++
@@ -1,2 +0,0 @@
-// Generated automatically by `build-script`.
-var projects = [{name: 'fend-neighborhood-map', live: true}, {name: 'fend-design-patterns', live: false}, {name: 'fend-feed-reader', live: false}, {name: 'fend-frogger', live: true}, {name: 'fend-portfolio', live: true}, {name: 'fend-resume', live: true}, {name: 'fend-optimization', live: false}];
