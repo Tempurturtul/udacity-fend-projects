45650b13561e56f2df91a5de00153b079ad79ef4
Tempurturtul <tempurturtul@gmail.com>
2016-12-01 22:12:08 -0500

    Overwrite accidental neighborhood-map deploy

---
+++ scripts/projects.js
@@ -0,0 +1,2 @@
+// Generated automatically by `build-script`.
+var projects = [{name: 'fend-frogger', live: true}, {name: 'fend-neighborhood-map', live: true}, {name: 'fend-optimization', live: false}, {name: 'fend-feed-reader', live: false}, {name: 'fend-design-patterns', live: false}, {name: 'fend-resume', live: true}, {name: 'fend-portfolio', live: true}];
